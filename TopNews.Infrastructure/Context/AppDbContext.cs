1aa3ba035a23f3a5c2def395e34bb3391f872223
Pasha Mayba <pashamayba7@gmail.com>
2023-07-05 20:54:16 +0300

    Add project files.

---
+++ TopNews.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopNews.Infrastructure.Context
+{
+    internal class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(): base() {  }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+      
+    }
+}
---
+++ TopNews.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopNews.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
