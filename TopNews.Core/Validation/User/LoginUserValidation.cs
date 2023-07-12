3b8d60575ccb248b6f81345a5049a56a8a6a75b4
Pasha Mayba <pashamayba7@gmail.com>
2023-07-12 19:57:59 +0300

    Adding validation

---
+++ TopNews.Core/Validation/User/LoginUserValidation.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Core.DTOs.Login;
+
+namespace TopNews.Core.Validation.User
+{
+    public class LoginUserValidation: AbstractValidator<LoginDTO>
+    {
+        public LoginUserValidation()
+        {
+            RuleFor(r => r.Email).NotEmpty().WithMessage("Filed must not be empty")
+                .EmailAddress().WithMessage("Invalid email format.");
+            
+            RuleFor(r => r.Password).NotEmpty().WithMessage("Filed must not be empty")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+            
+        }
+    }
+}
--- TopNews.WEB/Controllers/DashboardController.cs
+++ TopNews.WEB/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TopNews.Core.DTOs.Login;
+using TopNews.Core.Validation.User;
 
 namespace TopNews.WEB.Controllers
 {
@@ -11,10 +13,28 @@ namespace TopNews.WEB.Controllers
             return View();
         }
 
-        [AllowAnonymous]
+        [AllowAnonymous]//GET
         public IActionResult Login()
         {
             return View();
         }
+     
+        [AllowAnonymous]//POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Login(LoginDTO model)
+        {
+            var validator = new LoginUserValidation();
+            var validatotionResult = validator.Validate(model);
+            if (validatotionResult.IsValid) 
+            {
+                return View();
+            }
+            ViewBag.AuthError = validatotionResult.Errors[0];
+            return View(model);
+            
+        }
+
     }
 }
