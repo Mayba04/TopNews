2c9abf1f4279eee34d2521bebffd9355ace469c1
Pasha Mayba <pashamayba7@gmail.com>
2023-08-24 11:12:07 +0300

    Adding ability to add categories for admin

---
+++ TopNews.Core/Validation/Category/CreateCategoryValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Core.DTOs.Category;
+
+namespace TopNews.Core.Validation.Category
+{
+    public class CreateCategoryValidation: AbstractValidator<CategoryDTO>
+    {
+        public CreateCategoryValidation()
+        {
+            RuleFor(r => r.Name).MinimumLength(2).NotEmpty().MaximumLength(12).WithMessage("The role name must not be empty, have more than 2 characters and no more than 12 characters");
+        }
+    }
+}
--- TopNews.WEB/Controllers/CategoryController.cs
+++ TopNews.WEB/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TopNews.Core.DTOs.Category;
 using TopNews.Core.Services;
+using TopNews.Core.Validation.Category;
+using TopNews.Core.Validation.User;
 
 namespace TopNews.WEB.Controllers
 {
@@ -22,9 +25,40 @@ namespace TopNews.WEB.Controllers
 
         public async Task<IActionResult> GetAllCategory()
         {
-            var result = await _categoryService.GetAll();
-            return View(result);
+            var categoriesTask = await _categoryService.GetAll();
+
+            return View(categoriesTask);
+
         }
+
+        public async Task<IActionResult> CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategory(CategoryDTO model)
+        {
+            var validator = new CreateCategoryValidation();
+            var validationResult = await validator.ValidateAsync(model);
+            if (validationResult.IsValid)
+            {
+                var categoriesTask = _categoryService.GetAll();
+                List<CategoryDTO> categories = await categoriesTask;
+                bool containsCategory = categories.Any(cat => cat.Name == model.Name);
+                if (containsCategory) 
+                {
+                    ViewBag.AuthError = "Such a category already exists";
+                    return View();
+                }
+                await _categoryService.Create(model);
+                return View(nameof(GetAllCategory));
+            }
+            ViewBag.AuthError = validationResult.Errors[0];
+            return View();
+        }
+
 
     }
 }
