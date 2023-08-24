9da01ef5dc12579b54c0ea4e67adf1faf726feb4
Pasha Mayba <pashamayba7@gmail.com>
2023-08-24 15:54:36 +0300

    Adding ability for admin to update categories

--- TopNews.WEB/Controllers/CategoryController.cs
+++ TopNews.WEB/Controllers/CategoryController.cs
@@ -11,7 +11,7 @@ namespace TopNews.WEB.Controllers
         private readonly UserService _userService;
         private readonly CategoryService _categoryService;
 
-      
+
         public CategoryController(UserService userService, CategoryService categoryService)
         {
             _userService = userService;
@@ -47,7 +47,7 @@ namespace TopNews.WEB.Controllers
                 var categoriesTask = _categoryService.GetAll();
                 List<CategoryDTO> categories = await categoriesTask;
                 bool containsCategory = categories.Any(cat => cat.Name == model.Name);
-                if (containsCategory) 
+                if (containsCategory)
                 {
                     ViewBag.AuthError = "Such a category already exists";
                     return View();
@@ -61,8 +61,7 @@ namespace TopNews.WEB.Controllers
 
         public async Task<IActionResult> DeleteCategory(int id)
         {
-
-            CategoryDTO? result = await _categoryService.Get(id);
+            var result = await _categoryService.Get(id);
             if (result != null)
             {
                 return View(result);
@@ -77,10 +76,41 @@ namespace TopNews.WEB.Controllers
             return RedirectToAction(nameof(GetAllCategory));
         }
 
-        
+        public async Task<IActionResult> UpdateCategory(int Id)
+        {
+            var result = await _categoryService.Get(Id);
+            if (result != null)
+            {
+                return View(result);
+            }
+            ViewBag.AuthError = "An error occurred";
+            return RedirectToAction(nameof(GetAllCategory));
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateCategory(CategoryDTO model)
+        {
+            //var validator = new CreateCategoryValidation();
+            //var validationResult = await validator.ValidateAsync(model);
+            //if (validationResult.IsValid)
+            //{
+            //    var categoriesTask = _categoryService.GetAll();
+            //    List<CategoryDTO> categories = await categoriesTask;
+            //    bool containsCategory = categories.Any(cat => cat.Name == model.Name && cat.Id != model.Id );
+            //    if (containsCategory)
+            //    {
+            //        ViewBag.AuthError = "Such a category already exists";
+            //        return View();
+            //    }
+            //    await _categoryService.Update(model);
+            //    return RedirectToAction(nameof(GetAllCategory));
+            //}
+            //ViewBag.AuthError = validationResult.Errors[0];
+            //return View();
+            await _categoryService.Update(model);
+            return RedirectToAction(nameof(GetAllCategory));
 
-
-
+        }
     }
 }
