9c9fbabe8a5b07661f88bc1ebb9ffe55250c7c5d
Pasha Mayba <pashamayba7@gmail.com>
2023-08-25 20:46:16 +0300

    Added Post Controller

---
+++ TopNews.WEB/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TopNews.Core.Services;
+
+namespace TopNews.WEB.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly UserService _userService;
+        private readonly CategoryService _categoryService;
+
+
+        public PostController(UserService userService, CategoryService categoryService)
+        {
+            _userService = userService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
