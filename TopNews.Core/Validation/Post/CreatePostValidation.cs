d47485254f19e57dc246248a897a57b21f04e039
Pasha Mayba <pashamayba7@gmail.com>
2023-08-29 14:07:58 +0300

    Adding the ability to add a post for the administrator

---
+++ TopNews.Core/Validation/Post/CreatePostValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopNews.Core.DTOs.Category;
+using TopNews.Core.DTOs.Post;
+
+namespace TopNews.Core.Validation.Post
+{
+    public class CreatePostValidation : AbstractValidator<PostDTO>
+    {
+        public CreatePostValidation()
+        {
+            RuleFor(r => r.Title).MinimumLength(2).NotEmpty().WithMessage("The title must not be empty and have more than 2 characters ");
+            RuleFor(r => r.Description).MinimumLength(2).NotEmpty().WithMessage("The Description must not be empty and have more than 2 characters ");
+            RuleFor(r => r.Text).MinimumLength(2).NotEmpty().WithMessage("The Text must not be empty and have more than 2 characters ");
+        }
+    }
+}
--- TopNews.WEB/Controllers/PostController.cs
+++ TopNews.WEB/Controllers/PostController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TopNews.Core.DTOs.Category;
+using TopNews.Core.DTOs.Post;
 using TopNews.Core.Interfaces;
 using TopNews.Core.Services;
-
+using TopNews.Core.Validation.Post;
 
 namespace TopNews.WEB.Controllers
 {
@@ -29,5 +32,35 @@ namespace TopNews.WEB.Controllers
 
             return View(categoriesTask);
         }
+
+        public async Task<IActionResult> AddPost()
+        {
+            await LoadCategory();
+            return View();
+        }
+
+        private async Task LoadCategory()
+        {
+            List<CategoryDTO> result = await _categoryService.GetAll();
+            @ViewBag.Category = new SelectList((System.Collections.IEnumerable)result,
+                nameof(IdentityRole.Name), nameof(IdentityRole.Name)
+              );
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddPost(PostDTO model)
+        {
+            var validator = new CreatePostValidation();
+            var validationResult = await validator.ValidateAsync(model);
+            if (validationResult.IsValid)
+            {
+               await _postService.Create(model);
+               return RedirectToAction(nameof(GetAllPost));
+            }
+            ViewBag.AuthError = validationResult.Errors[0];
+            return View();
+        }
+
     }
 }
